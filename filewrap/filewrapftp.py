f386f46e015392a438f61cd1c51ce87501517285
Piotr Glinianowicz <piotr@ixsystems.com>
2016-09-23 19:44:00 +0200

    Fixed issues in FileWrapFtp caused by introducing FileType(Enum)

--- filewrap/filewrapftp.py
+++ filewrap/filewrapftp.py
@@ -15,14 +15,26 @@ class FileWrapFtp(FileWrapBase):
         with FTP(self.hostname, self.user, self.password) as ftp:
             ftp.cwd(self.path)
             for (name, t) in ftp.mlsd(facts=['type']):
-                if t['type'] in (FileType.dir, FileType.file):
+                if t['type'] in (FileType.dir.name, FileType.file.name):
                     yield FileWrapFtp(PurePath(self.uri).joinpath(name), type=t['type'], user=self.user, password=self.password)
                 else:
                     continue
 
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, val):
+        self._ftp_mappings = {
+            'dir': FileType.dir,
+            'file': FileType.file,
+        }
+        self.__type = self._ftp_mappings[val] if val else None
+
     def _get_type(self):
         """ TODO """
-        return FileType.dir
+        return 'dir'
 
     def _get_parent(self):
         return FileWrapFtp(PurePath(self.uri).parent.as_posix(), user=self.user, password=self.password)
